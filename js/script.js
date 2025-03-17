41009486dab353141894dae287b023410807bd6a
MAX50K <146106579+MAX50K@users.noreply.github.com>
2025-03-17 16:47:02 +0100

    nice

--- js/script.js
+++ js/script.js
@@ -1,17 +1,46 @@
 // 1
 function logItems (a, b, c, d, e, f) {
-    
+    for (let i = 0; i < logItems.length; i++) {
+        console.log(`${i + 1} - ${logItems[i]}`);
+    }
 }
 logItems = ["42", "Chaos", "Omega", "99", "Phantom", "Void"]
-for (let i = 0; i < logItems.length; i++) {
-    console.log(`${i + 1} - ${logItems[i]}`);
-}
+
 console.log("---------------------------------------------");
 // 2
 function calculateEngravingPrice(message, pricePerWord) {
     const words = message.split(" "); 
     return words.length * pricePerWord; 
 }
-
 console.log(calculateEngravingPrice("Я люблю цукерки", 10)); 
-console.log(calculateEngravingPrice("Героям слава", 15));  
+console.log(calculateEngravingPrice("Пташки літають в небі", 15));
+console.log("---------------------------------------------");
+// 3
+function findLongestWord(sentence) {
+    let words = string.split(" ");
+    let sentence = "";
+    
+    for (let word of words) {
+        if (word.length >= sentence.length) {
+            sentence = word;
+        }
+    }
+    
+    return sentence;
+}
+console.log(findLongestWord("Cвітло місяця останього дня освітить замкову цілину."));
+console.log("---------------------------------------------");
+// 4
+function formatString(longSentence) {
+    return longSentence.length <= 40 ? longSentence : longSentence.slice(0, 40) + '...';
+}
+
+console.log(formatString("Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam aut blanditiis totam?"));
+// 5
+function checkForSpam(DiscordMessage) {
+    if (DiscordMessage.includes("spam" || "sale")) {
+        return true;
+    } else {
+        return false;
+    }
+}
