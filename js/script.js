2fd0367a886ed42ca965192cc5f4d4678228e681
MAX50K <146106579+MAX50K@users.noreply.github.com>
2025-03-19 17:13:18 +0100

    L)

--- js/script.js
+++ js/script.js
@@ -17,30 +17,68 @@ console.log(calculateEngravingPrice("Пташки літають в небі", 1
 console.log("---------------------------------------------");
 // 3
 function findLongestWord(sentence) {
-    let words = string.split(" ");
-    let sentence = "";
+    let words = sentence.split(" ");
+    let longestWord = "";
     
     for (let word of words) {
-        if (word.length >= sentence.length) {
-            sentence = word;
+        if (word.length > longestWord.length) {
+            longestWord = word;
         }
     }
     
-    return sentence;
+    return longestWord;
 }
-console.log(findLongestWord("Cвітло місяця останього дня освітить замкову цілину."));
+let sentence = "Cвітло місяця останього дня освітить замкову цілину.";
+console.log(findLongestWord(sentence));
 console.log("---------------------------------------------");
 // 4
 function formatString(longSentence) {
     return longSentence.length <= 40 ? longSentence : longSentence.slice(0, 40) + '...';
 }
 
-console.log(formatString("Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam aut blanditiis totam?"));
+console.log(formatString("Lorem ipsum dolor sit amet consectetur adipisicing elit."));
+console.log("---------------------------------------------");
 // 5
-function checkForSpam(DiscordMessage) {
-    if (DiscordMessage.includes("spam" || "sale")) {
+function checkForSpam(message) {
+    if (message.includes("spam")) {
+        return true;
+    } else if (message.includes("sale")){
         return true;
     } else {
         return false;
     }
 }
+let message = "Hey! We are curently putting dress on sale, so if you are..."
+
+console.log(checkForSpam(message));
+console.log("---------------------------------------------");
+// 6
+let input;
+const numbers = [];
+let total = 0;
+
+while (true) {
+  input = prompt("Enter a number:");
+
+  if (input === null) {
+    break;
+  }
+
+  const number = parseFloat(input);
+  if (isNaN(number)) {
+    alert("There was not a number that was entered. Please try again.");
+  } else {
+    numbers.push(number);
+  }
+}
+
+if (numbers.length > 0) {
+  for (let number of numbers) {
+    total += number;
+  }
+  console.log(`The total is = ${total}`);
+} else {
+  console.log("The list is empty.");
+}
+console.log("---------------------------------------------");
+// 7
